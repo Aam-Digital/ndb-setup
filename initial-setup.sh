c420db5c1849225ddb90622f8fc5d5a5226beff2
Sebastian Leidig <sebastian.leidig@gmail.com>
2020-07-21 16:50:09 +0200

    add WEBDAV_URL environment as required by 2.14.5

--- initial-setup.sh
+++ initial-setup.sh
@@ -2,18 +2,18 @@
 #       couchdb_port
 #       couchdb_password
 if ! [ $# -eq 2 ]; then
-        echo "Usage: $0 couchdb_port couchdb_admin_password"
+        echo "Usage: $0 aam_url couchdb_admin_password"
         exit
 fi
 
 # Create system databases for couchdb
-curl -X PUT -u admin:$2 https://$1/db/_users
-curl -X PUT -u admin:$2 https://$1/db/_replicator
-curl -X PUT -u admin:$2 https://$1/db/_global_changes
+curl -X PUT -u admin:$2 $1/db/_users
+curl -X PUT -u admin:$2 $1/db/_replicator
+curl -X PUT -u admin:$2 $1/db/_global_changes
 
 # create app database
-curl -X PUT -u admin:$2 https://$1/db/app
-curl -X PUT -u admin:$2 https://$1/db/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
+curl -X PUT -u admin:$2 $1/db/app
+curl -X PUT -u admin:$2 $1/db/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
 
 
 # add users

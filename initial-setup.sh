4c527c4985c1483c744bc266dd01bea74dacaa86
Simon <therealslimv@yahoo.de>
2022-02-15 15:18:30 +0100

    review changes

--- initial-setup.sh
+++ initial-setup.sh
@@ -14,6 +14,7 @@ curl -X PUT -u admin:$2 $1/db/_global_changes
 
 # create app database
 curl -X PUT -u admin:$2 $1/db/app
+#curl -X PUT -u admin:$2 $1/db/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
 
 # add users
 #js ./ndb-admin/add-user.js admin:ADMIN_PASSWORD app USER USER_PASSWORD

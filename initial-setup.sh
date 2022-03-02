a4cb06202470b071d83509807cda5ef4855d00bc
Simon <therealslimv@yahoo.de>
2022-03-02 17:19:51 +0100

    improved initial setup script to work directly on the couchdb behind the proxy

--- initial-setup.sh
+++ initial-setup.sh
@@ -8,15 +8,13 @@ if ! [ $# -ge 2 ]; then
         exit
 fi
 
-# Create system databases for couchdb
-curl -X PUT -u admin:$2 $1/db/_users
-curl -X PUT -u admin:$2 $1/db/_replicator
-curl -X PUT -u admin:$2 $1/db/_global_changes
-
-# create app database
-curl -X PUT -u admin:$2 $1/db/app
-
 if  [ $# -eq 2 ]; then
+  # Create system databases for couchdb
+  curl -X PUT -u admin:$2 $1/db/_users
+  curl -X PUT -u admin:$2 $1/db/_replicator
+  curl -X PUT -u admin:$2 $1/db/_global_changes
+  curl -X PUT -u admin:$2 $1/db/app
+
   # no backend defined, allowing 'user_app' to access 'app' database and modify their own user object
   curl -X PUT -u admin:$2 $1/db/_users/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
   curl -X PUT -u admin:$2 $1/db/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
@@ -24,10 +22,15 @@ if  [ $# -eq 2 ]; then
   exit
 fi
 
+# Create system databases for couchdb behind proxy
+curl -X PUT -u admin:$2 $1/db/couchdb/_users
+curl -X PUT -u admin:$2 $1/db/couchdb/_replicator
+curl -X PUT -u admin:$2 $1/db/couchdb/_global_changes
+curl -X PUT -u admin:$2 $1/db/couchdb/app
 # setup replicator user and give it access to 'app' and '_users' database
-curl -X PUT -u admin:$2 $1/db/_users/org.couchdb.user:replicator -d '{"name": "replicator", "password": "'$3'", "roles": [], "type": "user"}'
-curl -X PUT -u admin:$2 $1/db/_users/_security -d '{"admins": { "names": ["replicator"], "roles": [] }, "members": { "names": [], "roles": ["_admin"] } }'
-curl -X PUT -u admin:$2 $1/db/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": ["replicator"], "roles": ["_admin"] } }'
+curl -X PUT -u admin:$2 $1/db/couchdb/_users/org.couchdb.user:replicator -d '{"name": "replicator", "password": "'$3'", "roles": [], "type": "user"}'
+curl -X PUT -u admin:$2 $1/db/couchdb/_users/_security -d '{"admins": { "names": ["replicator"], "roles": ["_admin"] }, "members": { "names": [], "roles": [] } }'
+curl -X PUT -u admin:$2 $1/db/couchdb/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": ["replicator"], "roles": ["_admin"] } }'
 
 # add users
 #js ./ndb-admin/add-user.js admin:ADMIN_PASSWORD app USER USER_PASSWORD

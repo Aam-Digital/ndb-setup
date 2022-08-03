29000d6a0689109ebfa721652a182f082e2dff65
Simon <simon@aam-digital.com>
2022-08-03 08:38:33 +0200

    fixed create_realm script

--- keycloak/create_realm.sh
+++ keycloak/create_realm.sh
@@ -6,17 +6,18 @@ fi
 
 # Copy client config file to docker
 docker cp ./client_config.json "$1":client_config.json
-docker exec -it "$1" /bin/sh
 
+cat <<EOF | docker exec -i "$1" /bin/sh
 # Commands inside docker image
-export PATH=$PATH:/opt/keycloak/bin
+export PATH=\$PATH:/opt/keycloak/bin
 # Login
-kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password "$2"
+/opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password "$2"
 # Create realm
-kcadm.sh create realms -s realm="$3" -s enabled=true -o
+/opt/keycloak/bin/kcadm.sh create realms -s realm="$3" -s enabled=true -o
 # Set aam theme
-kcadm.sh update realms/"$3" -s loginTheme=aam-theme
+/opt/keycloak/bin/kcadm.sh update realms/"$3" -s loginTheme=aam-theme
 # Create client from config file and store ID
-CID=$(kcadm.sh create clients -r "$3" -f /client_config.json -i)
-# Create default `user_app` role
-kcadm.sh create clients/"$CID"/roles -r $3 -s name=user_app -s 'description=Regular user with DB access'
+CID=\$(/opt/keycloak/bin/kcadm.sh create clients -r "$3" -f /client_config.json -i)
+# Create default user_app role
+/opt/keycloak/bin/kcadm.sh create clients/"\$CID"/roles -r "$3" -s name=user_app -s 'description=Regular user with DB access'
+EOF

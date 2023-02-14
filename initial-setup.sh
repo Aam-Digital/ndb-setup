f51050fbc8202fcc0c2573f3baab213b7a86598a
Simon <simon@aam-digital.com>
2023-02-14 21:16:57 +0100

    added interactive setup script to setup app and backend

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,29 +1,66 @@
-# use with parameters:
-#       couchdb_port
-#       couchdb_password
-if ! [ $# -ge 2 ]; then
-        echo "Usage: $0 aam_url couchdb_admin_password"
-        echo "Example with backend: $0 https://demo.aam-digital.com <COUCHDB_PASSWORD> true"
-        echo "Example without backend: $0 https://demo.aam-digital.com <COUCHDB_PASSWORD>"
-        exit
+#!/bin/bash
+chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+generate_password() {
+  password=""
+  for i in {1..16} ; do
+    password="$password${chars:RANDOM%${#chars}:1}"
+  done
+}
+
+org=${PWD##*/ndb-}
+app=$(docker compose images| grep -c couchdb)
+echo "App $app"
+if [ "$app" == 0 ]
+then
+  echo "Setting up new instance '$org'"
+
+  if [ ! -f ".env" ]
+  then
+    generate_password
+    echo "COUCHDB_PASSWORD=$password" >> .env
+    echo "Admin password: $password"
+    url=$org.aam-digital.net
+    echo "APP_URL=$url" >> .env
+    echo "App URL: $url"
+  fi
+  docker compose up -d
+
+  # wait for DB to be ready
+  source '.env'
+  while [ "$status" != 200 ]
+  do
+    sleep 1
+    echo "Waiting for DB to be ready"
+    status=$(curl --silent --output /dev/null  https://$APP_URL/db/_utils/ -I -w "%{http_code}\n")
+  done
+  curl -X PUT -u admin:$COUCHDB_PASSWORD https://$APP_URL/db/app
+
+else
+  echo "Instance '$org' already exists"
 fi
 
-# Create system databases for couchdb
-curl -X PUT -u admin:$2 $1/db/_users
-curl -X PUT -u admin:$2 $1/db/_replicator
-curl -X PUT -u admin:$2 $1/db/_global_changes
-curl -X PUT -u admin:$2 $1/db/app
+backend=$(docker compose images | grep -c backend)
+echo "backend $backend"
 
-if [ "$3" == "true" ]; then
-  # No need to update security documents
-  exit
+if [ "$backend" == 0 ]
+then
+  echo "Do you want to add the permission backend?[y/n]"
+  read -r withBackend
+  if [ "$withBackend" == "y" ] || [ "$withBackend" == "Y" ]
+  then
+    echo "Adding backend"
+    mv docker-compose.yml docker-compose-old.yml
+    cp docker-compose-backend.yml docker-compose.yml
+    generate_password
+    echo "JWT_SECRET=$password" >> .env
+    docker compose up -d
+    backend=1
+  elif [ "$app" == 0 ]
+  then
+    source '.env'
+    curl -X PUT -u admin:$COUCHDB_PASSWORD https://$APP_URL/db/_users
+    curl -X PUT -u admin:$COUCHDB_PASSWORD https://$APP_URL/db/_users/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
+    curl -X PUT -u admin:$COUCHDB_PASSWORD https://$APP_URL/db/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
+    echo "'user_app' has access to database 'app'"
+  fi
 fi
-
-# allowing 'user_app' to access 'app' database and modify their own user object
-curl -X PUT -u admin:$2 $1/db/_users/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
-curl -X PUT -u admin:$2 $1/db/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
-echo "'user_app' has access to 'app' database"
-exit
-
-# add users
-#js ./ndb-admin/add-user.js admin:ADMIN_PASSWORD app USER USER_PASSWORD

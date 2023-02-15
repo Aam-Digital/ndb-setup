f1d560045fc6492bc1e0f8ad988b42deb30b9e78
Simon <simon@aam-digital.com>
2023-02-15 16:11:29 +0100

    cleaned up setup and mounted config files for keycloak into container

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 generate_password() {
   password=""
@@ -8,8 +9,7 @@ generate_password() {
 }
 
 org=${PWD##*/ndb-}
-app=$(docker compose images| grep -c couchdb)
-echo "App $app"
+app=$(docker compose images 2>/dev/null | grep -c couchdb)
 if [ "$app" == 0 ]
 then
   echo "Setting up new instance '$org'"
@@ -25,7 +25,8 @@ then
     echo "COUCHDB_PASSWORD=$password" >> .env
     echo "Admin password: $password"
 
-    url=$org.aam-digital.net
+    # might need to be adjusted base on the domain
+    url=$org.aam-digital.com
     echo "APP_URL=$url" >> .env
     echo "App URL: $url"
   fi
@@ -46,55 +47,65 @@ else
 fi
 
 backend=$(docker compose images | grep -c backend)
-echo "backend $backend"
-
 if [ "$backend" == 0 ]
 then
   echo "Do you want to add the permission backend?[y/n]"
   read -r withBackend
   if [ "$withBackend" == "y" ] || [ "$withBackend" == "Y" ]
   then
-    echo "Adding backend"
-    # mv docker-compose.yml docker-compose-old.yml
+    mv docker-compose.yml docker-compose-old.yml
     cp docker-compose-backend.yml docker-compose.yml
     generate_password
     echo "JWT_SECRET=$password" >> .env
     docker compose up -d
     backend=1
-  fi
+    echo "Backend added"
+ fi
 fi
 
 if [ ! -f "keycloak.json" ]
 then
   echo "Do you want to add authentication via Keycloak?[y/n]"
   read -r keycloak
-  container=$(docker ps -aqf "name=keycloak-keycloak")
-  # This might need to be adjusted, depending where the keycloak is running
-  source "/var/docker/nginx-proxy/keycloak/.env"
-  source ".env"
-  docker exec -i "$container" /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password "$ADMIN_PASSWORD"
-  docker exec -i "$container" /opt/keycloak/bin/kcadm.sh create realms -s realm="$org" -f /realm_config.json -i
-  client=$(docker exec -i "$container" /opt/keycloak/bin/kcadm.sh create clients -r "$org" -s baseUrl="https://$APP_URL" -f /client_config.json -i)
-  token=$(curl --silent --location "https://$KEYCLOAK_URL/realms/master/protocol/openid-connect/token" --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode username=admin --data-urlencode password="$ADMIN_PASSWORD" --data-urlencode grant_type=password --data-urlencode client_id=admin-cli)
-  token=${token#*\"access_token\":\"}
-  token=${token%\",\"expires_in\"*}
-  curl --silent --location "https://$KEYCLOAK_URL/admin/realms/dev/clients/$client/installation/providers/keycloak-oidc-keycloak-json" --header "Authorization: Bearer $token" > keycloak.json
-  docker compose stop
-  sed -i "s/\"account_url\": \".*\"/\"account_url\": \"https:\/\/$ACCOUNTS_URL\"/g" config-keycloak.json
-  cp config-keycloak.json config.json
-  sed -i "s/\#\- .\/keycloak/\- .\/keycloak/g" docker-compose.yml
-  docker compose up -d
+  source '.env'
   if [ "$keycloak" == "y" ] || [ "$keycloak" == "Y" ]
   then
+    container=$(docker ps -aqf "name=keycloak-keycloak")
+    # This might need to be adjusted, depending where the keycloak is running
+    source "/var/docker/nginx-proxy/keycloak/.env"
+    # Initialize realm and client
+    docker exec -i "$container" /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password "$ADMIN_PASSWORD"
+    docker exec -i "$container" /opt/keycloak/bin/kcadm.sh create realms -s realm="$org" -f /realm_config.json -i
+    client=$(docker exec -i "$container" /opt/keycloak/bin/kcadm.sh create clients -r "$org" -s baseUrl="https://$APP_URL" -f /client_config.json -i)
+
+    # Get Keycloak config from API
+    token=$(curl --silent --location "https://$KEYCLOAK_URL/realms/master/protocol/openid-connect/token" --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode username=admin --data-urlencode password="$ADMIN_PASSWORD" --data-urlencode grant_type=password --data-urlencode client_id=admin-cli)
+    token=${token#*\"access_token\":\"}
+    token=${token%%\"*}
+    curl --silent --location "https://$KEYCLOAK_URL/admin/realms/dev/clients/$client/installation/providers/keycloak-oidc-keycloak-json" --header "Authorization: Bearer $token" > keycloak.json
+    sed -i "s/\"account_url\": \".*\"/\"account_url\": \"https:\/\/$ACCOUNTS_URL\"/g" config-keycloak.json
+    cp config-keycloak.json config.json
+    sed -i "s/\#\- .\/keycloak/\- .\/keycloak/g" docker-compose.yml
+
+    # Set Keycloak public key vor bearer auth
+    keys=$(curl --silent --location "https://$KEYCLOAK_URL/admin/realms/master/keys" --header "Authorization: Bearer $token")
+    kid=${keys#*\"RS256\":\"}
+    kid=${kid%%\"*}
+    keys=${keys#*\"algorithm\":\"RS256\",}
+    publicKey=${keys#*\"publicKey\":\"}
+    publicKey=${publicKey%%\"*}
     if [ "$backend" == 0 ]
     then
-      echo ""
+      sed -i "s/<KID>/$kid/g" couchdb.ini
+      sed -i "s|<PUBLIC_KEY>|$publicKey|g" couchdb.ini
     else
-      echo ""
+      echo "PUBLIC_KEY=$publicKey" >> .env
     fi
+    docker compose stop
+    docker compose up -d
+    echo "App is connected with Keycloak"
   elif [ "$app" == 0 ]
   then
-    source '.env'
     curl -X PUT -u admin:$COUCHDB_PASSWORD https://$APP_URL/db/_users
     curl -X PUT -u admin:$COUCHDB_PASSWORD https://$APP_URL/db/_users/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'
     curl -X PUT -u admin:$COUCHDB_PASSWORD https://$APP_URL/db/app/_security -d '{"admins": { "names": [], "roles": [] }, "members": { "names": [], "roles": ["user_app"] } }'

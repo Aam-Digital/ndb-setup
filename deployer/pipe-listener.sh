7df6c18e7a45c1dc41b95c89b4adf1f188fa32ae
Simon <simon@aam-digital.com>
2023-11-21 15:11:46 +0100

    forwarding pipe output to file

--- deployer/pipe-listener.sh
+++ deployer/pipe-listener.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # shellcheck disable=SC2046
-while true; do (cd /var/docker/setup; ./interactive_setup.sh $(cat deployer/arg-pipe)); done
+while true; do (cd /var/docker/setup; ./interactive_setup.sh $(cat deployer/arg-pipe) &> deploy-log.txt); done

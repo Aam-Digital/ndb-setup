5cf00bbaa89f4466e2a550e5ca96f9f75b42b172
Tom Winter <tom@aam-digital.com>
2024-07-11 11:10:30 +0200

    fix: prevent errors in pipe-listener.sh

--- deployer/pipe-listener.sh
+++ deployer/pipe-listener.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# shellcheck disable=SC2046
-while true; do (cd /var/docker/setup; ./interactive_setup.sh $(cat deployer/arg-pipe) >> deployer/log.txt 2>&1 ); done
+while true; do (cd /var/docker/ndb-setup || exit; ./interactive_setup.sh "$(cat deployer/arg-pipe)" >> deployer/log.txt 2>&1 ); done

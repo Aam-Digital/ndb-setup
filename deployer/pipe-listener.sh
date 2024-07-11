277e16b03d8e3ba0663168e72150ddc4dbcea67c
Tom Winter <tom@aam-digital.com>
2024-07-11 11:46:37 +0200

    fix: remove argument escaping

--- deployer/pipe-listener.sh
+++ deployer/pipe-listener.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-while true; do (cd /var/docker/ndb-setup || exit; ./interactive_setup.sh "$(cat deployer/arg-pipe)" >> deployer/log.txt 2>&1 ); done
+# shellcheck disable=SC2046
+while true; do (cd /var/docker/ndb-setup || exit; ./interactive_setup.sh $(cat deployer/arg-pipe) >> deployer/log.txt 2>&1 ); done

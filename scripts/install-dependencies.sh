45e518ec0eabeb76b9ef1bb022d35621f1f286d1
Tom Winter <tom@aam-digital.com>
2024-12-13 12:29:45 +0100

    feat: add swag-reverse proxy and improve interactive_setup.sh (#48)

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# dependencies:
+# - Rust and Cargo
+# - Bitwarden Secrets Manager CLI
+
+sudo apt install -y build-essential
+
+# install rust and cargo (directly, without homebrew)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# configure current shell
+. "$HOME/.cargo/env"
+
+# install Bitwarden Secrets Manager CLI https://github.com/bitwarden/sdk/tree/main/crates/bws
+cargo install bws

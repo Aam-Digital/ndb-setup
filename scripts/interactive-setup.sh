00796e4f66c7eba5bedb9f0b02baf2f5521d62b9
Tom Winter <tom@aam-digital.com>
2025-04-01 09:54:32 +0200

    fix: use github api call to fetch template

--- scripts/interactive-setup.sh
+++ scripts/interactive-setup.sh
@@ -421,7 +421,7 @@ if [ "$aamBackendService" == 0 ]; then
     mkdir -p "$path/config/aam-backend-service"
 
     # copy latest template config (from aam-services repository)
-    curl -L -o "$path/config/aam-backend-service/application.env" "https://github.com/Aam-Digital/aam-services/blob/aam-backend-service/$backendVersion/templates/aam-backend-service/application.template.env"
+    curl -L -o "$path/config/aam-backend-service/application.env" "https://raw.githubusercontent.com/Aam-Digital/aam-services/refs/tags/aam-backend-service/$backendVersion/templates/aam-backend-service/application.template.env"
 
     generate_password
 

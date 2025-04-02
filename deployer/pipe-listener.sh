d2d9b7bb6e23fa317f3f161d3f6e0c3639731791
Tom Winter <tom@aam-digital.com>
2025-04-02 14:48:04 +0200

    fix: relative paths

--- deployer/pipe-listener.sh
+++ deployer/pipe-listener.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # shellcheck disable=SC2086
-while true; do (cd /var/docker/ndb-setup || exit; while read -r line; do ../scripts/interactive_setup.sh ${line}; done<deployer/arg-pipe ); done
+while true; do (cd /var/docker/ndb-setup/scripts || exit; while read -r line; do ./interactive-setup.sh ${line}; done<deployer/arg-pipe ); done

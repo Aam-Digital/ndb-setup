d7b237c69a3ca72ea38964dd80d6aedde27011b5
Tom Winter <tom@aam-digital.com>
2025-04-07 11:15:40 +0200

    fix: 2fa support

--- scripts/interactive-setup.sh
+++ scripts/interactive-setup.sh
@@ -313,7 +313,7 @@ if [ ! -f "$path/keycloak.json" ]; then
     curl -X DELETE "https://$KEYCLOAK_HOST/admin/realms/$org/users/$userId/role-mappings/realm" \
       -H "Authorization: Bearer $token" \
       -H "Content-Type: application/json" \
-      -d '[{ "name": "no-email-2fa" }]'
+      -d '{ "name": "no-email-2fa" }'
     echo "verify email..."
     curl -X PUT -s -H "Authorization: Bearer $token" -H 'Content-Type: application/json' -d '["VERIFY_EMAIL"]' "https://$KEYCLOAK_HOST/admin/realms/$org/users/$userId/execute-actions-email?client_id=app&redirect_uri="
     echo "create user document in couchdb..."

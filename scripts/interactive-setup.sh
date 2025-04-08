79b10b987b0f670a8e9ccfed3598b8986c48c603
Tom Winter <tom@aam-digital.com>
2025-04-08 11:46:55 +0200

    fix: delete role request

--- scripts/interactive-setup.sh
+++ scripts/interactive-setup.sh
@@ -309,9 +309,9 @@ if [ ! -f "$path/keycloak.json" ]; then
     curl -X PUT -s -H "Authorization: Bearer $token" -H 'Content-Type: application/json' -d '["VERIFY_EMAIL"]' "https://$KEYCLOAK_HOST/admin/realms/$org/users/$userId/execute-actions-email?client_id=app&redirect_uri="
 
     echo "enable 2fa for user..."
-    curl -X GET "https://$KEYCLOAK_HOST/realms/$org/roles" -H "Authorization: Bearer $token" | jq .
+    curl -X GET "https://$KEYCLOAK_HOST/admin/realms/$org/roles" -H "Authorization: Bearer $token" | jq .
 
-    roleId=$(curl -X GET "https://$KEYCLOAK_HOST/realms/$org/roles" -H "Authorization: Bearer $token" | jq -r '.[] | select(.name=="no-email-2fa") | .id')
+    roleId=$(curl -X GET "https://$KEYCLOAK_HOST/admin/realms/$org/roles" -H "Authorization: Bearer $token" | jq -r '.[] | select(.name=="no-email-2fa") | .id')
 
     echo "$roleId"
 

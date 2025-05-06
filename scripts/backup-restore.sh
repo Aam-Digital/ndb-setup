4d7176874bf334d95d365354173a11eba379b364
Sebastian <sebastian@aam-digital.com>
2025-05-06 09:27:29 +0200

    fix(backup): better logging for backup scripts

--- scripts/backup-restore.sh
+++ scripts/backup-restore.sh
@@ -7,26 +7,36 @@ source "$baseDirectory/ndb-setup/setup.env"
 backupRoot=$BACKUP_DIR
 passphrase=$BACKUP_PASSPHRASE
 
-echo "For which instance do you want to import the backup?"
-read -r org
-
-folder=c-$org
-
 echo "The backup of which day to you want to import? (Format YYYYMMDD e.g. 20220101)"
 read -r date
 
 cd $backupRoot
-echo "$passphrase" | gpg --batch --yes --passphrase-fd 0 -o output -d $date.tar.gz.gpg
-mkdir ~/unpacked
-tar -xzvf output --directory ~/unpacked
 
+echo "decrypting backup ..."
+echo "$passphrase" | gpg --batch --yes --passphrase-fd 0 -o output -d $date.tar.gz.gpg
+
+echo "unpacking backup ..."
+mkdir ~/unpacked
+tar -xzf output --directory ~/unpacked
+
+read -p "Do you want to restore a specific system? [y/n] " choice
+if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
+    echo "Archive unpacked to ~/unpacked. Stopping here."
+    exit 0
+fi
+
+echo "For which instance do you want to import the backup?"
+read -r org
+folder=c-$org
 cd /var/docker/$folder
 docker compose down
 mv couchdb couchdb_tmp
 mv ~/unpacked/var/docker/$folder/couchdb ./couchdb
 docker compose up -d
+echo "backup restored for $folder and instance has been restarted"
 
-#echo "Delete the temporary data?[y/n]"
+
+#echo "Delete the temporary data? [y/n]"
 #read -r delete
 #if [ "$delete" == "y" ] || [ "$delete" == "Y" ]; then
 #    rm -r couchdb_tmp
--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,6 +9,7 @@ backupRoot=$BACKUP_DIR
 passphrase=$BACKUP_PASSPHRASE
 
 targetFile=$backupRoot/`date +%Y%m%d`
+echo "Creating backup $targetFile ($(date '+%Y-%m-%d %H:%M:%S')) ..."
 
 tar zcf $targetFile.tar.gz /var/docker
 gpg -c --batch --passphrase "$passphrase" $targetFile.tar.gz
@@ -21,5 +22,6 @@ keep=14 #set this to how many files want to keep
 cd $backupRoot
 discard=$(expr $keep - $(ls|wc -l))
 if [ $discard -lt 0 ]; then
+  echo "Removing $discard old backups ..."
   ls -Bt|tail $discard|tr '\n' '\0'|xargs -0 printf "%b\0"|xargs -0 rm --
 fi

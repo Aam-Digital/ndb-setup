f8df5d18e32c2d25fa35baa5a3e4aae5f4d94c61
Sebastian Leidig <sebastian@aam-digital.com>
2025-06-02 15:02:43 +0200

    fix(scripts): avoid confusion about backup restore location

--- scripts/backup-restore.sh
+++ scripts/backup-restore.sh
@@ -10,18 +10,20 @@ passphrase=$BACKUP_PASSPHRASE
 echo "The backup of which day to you want to import? (Format YYYYMMDD e.g. 20220101)"
 read -r date
 
-cd $backupRoot
+cd "$backupRoot"
 
 echo "decrypting backup ..."
 echo "$passphrase" | gpg --batch --yes --passphrase-fd 0 -o output -d $date.tar.gz.gpg
 
-echo "unpacking backup ..."
-mkdir ~/unpacked
-tar -xzf output --directory ~/unpacked
+unpackDir="$baseDirectory/_backup_$date"
+echo "unpacking backup to $unpackDir ..."
+mkdir "$unpackDir"
+tar -xzf output --directory "$unpackDir"
+rm output
 
 read -p "Do you want to restore a specific system? [y/n] " choice
 if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
-    echo "Archive unpacked to ~/unpacked. Stopping here."
+    echo "Archive unpacked to $unpackDir. Stopping here."
     exit 0
 fi
 
@@ -31,7 +33,7 @@ folder=c-$org
 cd /var/docker/$folder
 docker compose down
 mv couchdb couchdb_tmp
-mv ~/unpacked/var/docker/$folder/couchdb ./couchdb
+mv "$unpackDir/var/docker/$folder/couchdb" ./couchdb
 docker compose up -d
 echo "backup restored for $folder and instance has been restarted"
 

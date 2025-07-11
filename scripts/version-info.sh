a5f79f6a72da536f0efc8ff4efefde8facff16d2
Sebastian Leidig <sebastian@aam-digital.com>
2025-07-11 15:12:45 +0200

    fix(scripts): error handling in versions script

--- scripts/version-info.sh
+++ scripts/version-info.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+
+##############################
+# setup
+##############################
+
+baseDirectory="/var/docker"
+source "$baseDirectory/ndb-setup/setup.env"
+
 # Funktion zum Abrufen der Umgebungsvariablen
 getVar() {
     local file="$1"
     local var="$2"
     local value
+
+    # Überprüfen, ob die Datei existiert
+    if [ ! -f "$file" ]; then
+        echo "n/a"
+        return
+    fi
 
     # grep sucht die Zeile mit der Variable, cut extrahiert den Wert
     value=$(grep "^$var=" "$file" | cut -d '=' -f2-)
@@ -36,10 +50,12 @@ getComposeProfiles() {
 {
 echo -e "instance-name \t deployment-type \t app-version \t backend-version \t export-api \t skilllab-api \t notification-api \t change-detection"
 echo -e "------------- \t --------------- \t ----------- \t --------------- \t ---------- \t ------------ \t ---------------- \t ----------------"
+
+cd "$baseDirectory" || exit
 for D in *; do
-        if [ -d "${D}" ] && [[ $D == c-* ]]; then
+        if [ -d "${D}" ] && [[ $D == "$PREFIX"* ]]; then
                 cd "$D" || exit;
-                instance_name="${D#c-}"
+                instance_name="${D}"
 
                 echo -e -n "$instance_name \t"
                 echo -e -n "$(getVar .env COMPOSE_PROFILES) \t"
